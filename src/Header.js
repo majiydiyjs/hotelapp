ae0f643c619dd3324da322a8625d328c26853f3f
majiydiyjs <119089769+majiydiyjs@users.noreply.github.com>
2023-04-29 16:09:11 +0900

    first

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      <div className="App-header">
+        <img
+          className="hotelLogo"
+          src="https://cdn-icons-png.flaticon.com/512/3378/3378693.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
